b9bfcc24125b37d5783521a2d494ac7a205c90c2
Michael Forde <MichaelFo@datacom.co.nz>
2020-05-19 14:56:04 +1200

    Initial setup for API project

---
+++ StemExplorerAPI/StemExplorerAPI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StemExplorerAPI.Controllers
+{
+    [Route("api/Home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("HealthCheck")]
+        public ActionResult<string> HealthCheck()
+        {
+            return "API up and running";
+        }
+    }
+}
