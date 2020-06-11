65066671b363f72f4c68f752be20fbf9203638ce
Benjamin-Davies <bentendavies@gmail.com>
2020-06-12 11:12:14 +1200

    Remove testEnv

--- stem-explorer-ng/src/app/app.component.ts
+++ stem-explorer-ng/src/app/app.component.ts
@@ -13,13 +13,4 @@ export class AppComponent {
     private configService: ConfigService,
   ) { }
 
-  // This is just a demonstration that the config service works
-  // Please inject it in api service or similar to call api calls
-  // or wherever you need to use env variables.
-  // For local development, set env vars in src/env.js file.
-  // For production or running in docker, set is as env variable
-  // for docker container.
-  get testEnv(): string {
-    return this.configService.get<string>('API_ENDPOINT');
-  }
 }
