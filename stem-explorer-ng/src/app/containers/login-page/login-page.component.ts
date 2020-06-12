09afc607217a75b67fc7e093e9aade341f3141d0
Benjamin-Davies <bentendavies@gmail.com>
2020-06-12 12:00:15 +1200

    Fill in login page

--- stem-explorer-ng/src/app/containers/login-page/login-page.component.ts
+++ stem-explorer-ng/src/app/containers/login-page/login-page.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
-  ngOnInit() {
+  loginWithGoogle() {
+    // TODO: Show login dialog
+    console.warn('Not implemented');
+  }
+
+  navigateToRegister() {
+    // TODO: Navigate to register page
+    console.warn('Not implemented');
+  }
+
+  navigateToHomepage() {
+    this.router.navigateByUrl('');
   }
 
 }
