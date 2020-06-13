9afea7e9d34e8139e8480eb71829d77b6f13f965
Joseph <xsaltedd@gmail.com>
2020-06-13 12:23:09 +1200

    commit register/login haha

--- stem-explorer-ng/src/app/containers/register-page/register-page.component.ts
+++ stem-explorer-ng/src/app/containers/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
