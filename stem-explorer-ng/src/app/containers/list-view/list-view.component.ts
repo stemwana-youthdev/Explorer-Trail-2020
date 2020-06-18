2c48c6f0ce7a3de282bb440a6419a83f72c51f09
nvz838 <nvz838@gmail.com>
2020-06-18 17:29:29 +1200

    Open challenge details in dialog

--- stem-explorer-ng/src/app/containers/list-view/list-view.component.ts
+++ stem-explorer-ng/src/app/containers/list-view/list-view.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { Categories } from '../../shared/enums/categories.enum';
 import { Challenge } from '../../shared/models/challenge';
+import { MatDialog } from '@angular/material/dialog';
+import { ListViewDialogComponent } from '../../components/list-view-dialog/list-view-dialog.component';
 
 @Component({
   selector: 'app-list-view',
@@ -13,7 +15,7 @@ export class ListViewComponent implements OnInit {
   challenges: Challenge[] = [];
   Categories : any = Categories;
 
-  constructor(private service: ApiService) {
+  constructor(private service: ApiService, public dialog: MatDialog) {
    }
 
   getChallenges() {
@@ -21,6 +23,16 @@ export class ListViewComponent implements OnInit {
       this.challenges = res["challenges"]
       this.challenges.sort((a, b) => (a.title > b.title) ? 1 : -1)
       });
+  }
+
+  openDialog(challenge) {
+    this.dialog.open(ListViewDialogComponent, {
+      data: {
+        title: challenge.title,
+        category: Categories[challenge.category],
+        description: challenge.description
+      }
+    });
   }
 
   ngOnInit() {
--- stem-explorer-ng/src/app/shared/material.module.ts
+++ stem-explorer-ng/src/app/shared/material.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 
@@ -14,11 +15,13 @@ import { MatDividerModule } from '@angular/material/divider';
     MatTabsModule,
     MatListModule,
     MatDividerModule,
+    MatDialogModule
   ],
   exports: [
     MatTabsModule,
     MatListModule,
     MatDividerModule,
-  ]
+    MatDialogModule
+  ],
 })
 export class MaterialModule { }
