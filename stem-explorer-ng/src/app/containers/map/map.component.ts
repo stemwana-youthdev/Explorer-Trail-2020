b0ce78ad95743357700729b9598098090f22194c
June Chong <jhc19@students.waikato.ac.nz>
2020-06-22 12:35:30 +1200

    imported and added the google maps component under the containers folder; added comments on the process of getting the json data from the local json file

--- stem-explorer-ng/src/app/containers/map/map.component.ts
+++ stem-explorer-ng/src/app/containers/map/map.component.ts
@@ -7,23 +7,23 @@ import { ApiService } from 'src/app/shared/services/api.service';
 })
 export class MapComponent implements OnInit {
   center: google.maps.LatLngLiteral;
-  location: any[] = [];
-  constructor(private service: ApiService) {}
+  location: any[] = []; //local property to store the json data from getLocations
+  constructor(private service: ApiService) {} //added a dependency injection in order to use the getLocations method without creating an instance of the object
 
 
   ngOnInit() {
     // navigator.geolocation.getCurrentPosition(position => {
       this.center = {
         lat: -37.6934845,
-        lng: 176.1649924,
+        lng: 176.1649924, //set maps to the center of Tauranga
       }
       this.loadLocation();
     // })
   }
-  loadLocation() {
+  loadLocation() { //uses the ApiService to call on the getLocations method to open a listening stream to get the data from the json file
     this.service.getLocations().subscribe(l => {
       console.log(l);
-      this.location = l['location'];
+      this.location = l['location']; //store the data in a local property
     });
   }
 }
