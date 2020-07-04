51e70cdacb2339d072f41277d2e0ee594d76a479
Benjamin-Davies <bentendavies@gmail.com>
2020-07-04 13:38:28 +1200

    Implement withApiFallback

--- stem-explorer-ng/src/app/shared/services/api.service.ts
+++ stem-explorer-ng/src/app/shared/services/api.service.ts
@@ -1,38 +1,71 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, ReplaySubject } from 'rxjs';
 
 import { ConfigService } from 'src/app/config/config.service';
 import { Challenge } from '../models/challenge';
 import { Location } from '../models/location';
+import { environment } from 'src/environments/environment';
+import { flatMap, map, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ApiService {
-  private useApiFallback = true;
-
-  constructor(
-    private http: HttpClient,
-    private config: ConfigService,
-  ) {}
+  private readonly useApiFallback: Observable<boolean>;
 
   get apiEndpoint() {
     return this.config.get('API_ENDPOINT') as string;
   }
 
-  getChallenges() {
-    if (this.useApiFallback) {
-      return this.http.get('/assets/challenges.json') as Observable<Challenges>;
+  constructor(private http: HttpClient, private config: ConfigService) {
+    const subject = new ReplaySubject<boolean>(1);
+    if (!environment.production) {
+      this.http
+        .get(`${this.apiEndpoint}/Home/HealthCheck`, { responseType: 'text' })
+        .subscribe({
+          next() {
+            subject.next(false);
+          },
+          error() {
+            console.warn('Could not connect to api, using assets.');
+            subject.next(true);
+          },
+        });
     } else {
-      return this.http.get(`${this.apiEndpoint}/Challenge/GetChallenges`) as Observable<Challenges>;
+      subject.next(false);
     }
+    this.useApiFallback = subject;
+  }
+
+  withApiFallback<T>(
+    action: (fallback: boolean) => Observable<T>
+  ): Observable<T> {
+    return this.useApiFallback.pipe(flatMap(action));
+  }
+
+  getChallenges() {
+    return this.withApiFallback((fallback) => {
+      if (fallback) {
+        return this.http.get('/assets/challenges.json') as Observable<
+          Challenges
+        >;
+      } else {
+        return this.http.get(
+          `${this.apiEndpoint}/Challenge/GetChallenges`
+        ) as Observable<Challenges>;
+      }
+    });
   }
 
   getLocations() {
-    if (this.useApiFallback) {
-      return this.http.get('/assets/locations.json') as Observable<Locations>;
-    } else {
-      return this.http.get(`${this.apiEndpoint}/Location/GetLocations`) as Observable<Locations>;
-    }
+    return this.withApiFallback((fallback) => {
+      if (fallback) {
+        return this.http.get('/assets/locations.json') as Observable<Locations>;
+      } else {
+        return this.http.get(
+          `${this.apiEndpoint}/Location/GetLocations`
+        ) as Observable<Locations>;
+      }
+    });
   }
 }
 
