ad30db9d6b6db7453590447f259a77e434a3708a
nvz838 <nvz838@gmail.com>
2020-07-09 09:54:23 +1200

    add challenge to routing module

--- stem-explorer-ng/src/app/app-routing.module.ts
+++ stem-explorer-ng/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomePageComponent } from './containers/home-page/home-page.component';
 import { LoginPageComponent } from './containers/login-page/login-page.component';
 import { RegisterPageComponent } from './containers/register-page/register-page.component';
 import { ListViewComponent } from './containers/list-view/list-view.component';
+import { ChallengeViewComponent } from './components/challenge-view/challenge-view.component';
 
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegisterPageComponent},
-  { path: 'list-view', component: ListViewComponent}
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'list-view', component: ListViewComponent },
+  { path: 'challenge/:id', component: ChallengeViewComponent }
 ];
 
 @NgModule({
