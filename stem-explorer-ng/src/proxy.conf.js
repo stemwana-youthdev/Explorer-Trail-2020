fe709946b3aa2797f48cbc4bc11fec24c8f8e4c2
Benjamin-Davies <bentendavies@gmail.com>
2020-07-09 11:20:52 +1200

    Configure Angular proxy using env variables

--- stem-explorer-ng/src/proxy.conf.js
+++ stem-explorer-ng/src/proxy.conf.js
@@ -1,6 +1,9 @@
 module.exports = {
   '/api': {
+    target: process.env.API_HOST || 'http://localhost:5000',
     bypass(req) {
+      if (process.env.USE_FIXTURES !== 'true') return;
+
       switch (req.originalUrl) {
         case '/api/Challenge/GetChallenges':
           return '/assets/challenges.json';
--- stem-explorer-ng/src/proxy.docker.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '/api': {
-    target: 'http://explorer_trial_api:5000',
-  },
-};
