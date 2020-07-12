d9227ff6e4b85aee59c40ca6d77a84db322e3b68
Benjamin-Davies <bentendavies@gmail.com>
2020-07-12 20:59:48 +1200

    Add dev configs

--- stem-explorer-ng/src/proxy.conf.js
+++ stem-explorer-ng/src/proxy.conf.js
@@ -1,4 +1,7 @@
 module.exports = {
+  '/admin': {
+    target: process.env.ADMIN_URL || 'http://localhost:4300',
+  },
   '/api': {
     target: process.env.API_HOST || 'http://localhost:5000',
     bypass(req) {
