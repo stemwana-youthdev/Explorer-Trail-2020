3bc1b066f9725a2bcc202c5acf3c3fb969afbd04
Benjamin-Davies <bentendavies@gmail.com>
2020-07-14 18:18:20 +1200

    Add API_ENDPOINT env var to admin

--- stem-admin/run.sh
+++ stem-admin/run.sh
@@ -5,6 +5,6 @@ set -x
 export DOLLAR='$'
 
 envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-#envsubst < /var/www/src/env.js.template > /var/www/html/env.js
+envsubst < /var/www/src/env.js.template > /var/www/html/env.js
 
 nginx -g 'daemon off;'
