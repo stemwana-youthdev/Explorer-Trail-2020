4f9ae3f293ad06b61b28958f7fe9bedfa2970b74
nvz838 <nvz838@gmail.com>
2020-07-16 09:57:27 +1200

    button styles

--- stem-explorer-ng/src/app/components/button/button.component.ts
+++ stem-explorer-ng/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
+  @Input() colour: string;
+  
   constructor() { }
 
   ngOnInit(): void {
