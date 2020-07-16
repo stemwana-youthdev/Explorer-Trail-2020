8160bf12ab3e3aacc3bcab56c10f48fd4e436291
nvz838 <nvz838@gmail.com>
2020-07-16 14:12:04 +1200

    border colours

--- stem-explorer-ng/src/app/app.module.ts
+++ stem-explorer-ng/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { DrawerComponent } from './containers/drawer/drawer.component';
 import { HintDialogComponent } from './components/hint-dialog/hint-dialog.component';
 import { ButtonComponent } from './shared/components/button/button.component';
 import { CategoryButtonComponent } from './shared/components/category-button/category-button.component';
+import { CategoryCardComponent } from './shared/components/category-card/category-card.component';
 
 
 @NgModule({
@@ -55,7 +56,8 @@ import { CategoryButtonComponent } from './shared/components/category-button/cat
     DrawerComponent,
     HintDialogComponent,
     ButtonComponent,
-    CategoryButtonComponent
+    CategoryButtonComponent,
+    CategoryCardComponent
   ],
   imports: [
     GoogleMapsModule,
