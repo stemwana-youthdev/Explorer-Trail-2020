880aef89c8de19ecefbb17ac3cc94caa58608804
Benjamin-Davies <bentendavies@gmail.com>
2020-07-24 13:12:55 +1200

    Open the challenge dialog when the user scans a QR code

--- stem-explorer-ng/src/app/containers/camera/camera.component.ts
+++ stem-explorer-ng/src/app/containers/camera/camera.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ListViewDialogComponent } from 'src/app/components/list-view-dialog/list-view-dialog.component';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-camera',
@@ -9,7 +11,34 @@ import { Router } from '@angular/router';
 export class CameraComponent {
 
   constructor(
-    private router: Router,
+    private dialog: MatDialog,
+    private api: ApiService,
   ) { }
+
+  async scanSuccess(url: string) {
+    const regex = /^https?\:\/\/[\w\.]+\/challenge\/(\d+)$/;
+    const match = url.match(regex);
+    if (!match) {
+      // TODO: alert the user (probably small popup)
+      console.error('invalid qr code');
+      return;
+    }
+
+    const challengeId = parseInt(match[1], 10);
+    const challenges = await this.api.getChallenges().toPromise();
+    const challenge = challenges.challenges.find(c => c.uid === challengeId);
+    const locations = await this.api.getLocations().toPromise();
+    const location = locations.location.find(l => l.uid === challengeId);
+
+    this.dialog.closeAll();
+    this.dialog.open(ListViewDialogComponent, {
+      data: {
+        challenge,
+        name: location?.name,
+        link: location?.link,
+      },
+      panelClass: 'app-dialog',
+    });
+  }
 
 }
