cbd0e63c3ae5a1c63a31d5f6e29fce708542fdda
Benjamin-Davies <bentendavies@gmail.com>
2020-08-11 16:33:03 +1200

    Use ChallengeDifficulty in ChallengeLevelDto

--- StemExplorerAPI/StemExplorerAPI/Models/ViewModels/ChallengeLevelDto.cs
+++ StemExplorerAPI/StemExplorerAPI/Models/ViewModels/ChallengeLevelDto.cs
@@ -17,7 +17,7 @@ namespace StemExplorerAPI.Models.ViewModels
         [JsonPropertyName("uid")]
         public int Id { get; set; }
         public string QuestionText { get; set; }
-        public Enums.ChallengeLevel Difficulty { get; set; }
+        public Enums.ChallengeDifficulty Difficulty { get; set; }
         public string Instructions { get; set; }
         public Enums.AnswerType AnswerType { get; set; }
         public List<ChallengeAnswerDto> PossibleAnswers { get; set; }
