80587b694b62945b32b7bb04ab8060444ba9acd5
Benjamin-Davies <bentendavies@gmail.com>
2020-08-13 08:03:55 +1200

    Use the new locationdistance system for challengedialog

--- stem-explorer-ng/src/app/containers/challenge-dialog/challenge-dialog.component.ts
+++ stem-explorer-ng/src/app/containers/challenge-dialog/challenge-dialog.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Store } from '@ngxs/store';
-import { Observable, combineLatest } from 'rxjs';
+import { Observable, combineLatest, ReplaySubject, Subscription } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { CameraComponent } from '../camera/camera.component';
 
@@ -15,6 +15,7 @@ import { Challenge } from '../../shared/models/challenge';
 import { Location } from '../../shared/models/location';
 
 import { Categories } from '../../shared/enums/categories.enum';
+import { GeolocationService } from 'src/app/shared/services/geolocation.service';
 
 
 interface ChallengeDialogData {
@@ -29,20 +30,31 @@ interface ChallengeDialogData {
   templateUrl: './challenge-dialog.component.html',
   styleUrls: ['./challenge-dialog.component.scss'],
 })
-export class ChallengeDialogComponent implements OnInit {
+export class ChallengeDialogComponent implements OnInit, OnDestroy {
 
   Categories: any = Categories;
+
+  distance$: ReplaySubject<number>;
+  distanceSubscription: Subscription;
 
   constructor(
     private store: Store,
     private router: Router,
     @Inject(MAT_DIALOG_DATA) public data: ChallengeDialogData,
     private dialog: MatDialog,
+    private geolocation: GeolocationService,
   ) { }
 
   ngOnInit()  {
     this.store.dispatch(new LoadChallengesData());
     this.store.dispatch(new LoadLocationsData());
+
+    this.distance$ = new ReplaySubject();
+    this.distanceSubscription = this.getDistance().subscribe(this.distance$);
+  }
+
+  ngOnDestroy() {
+    this.distanceSubscription.unsubscribe();
   }
 
   get challenge$(): Observable<Challenge> {
@@ -60,6 +72,12 @@ export class ChallengeDialogComponent implements OnInit {
   get location$(): Observable<Location> {
     return this.store.select(LocationsState.challengeLocation).pipe(
       map((fn) => fn(this.data.challengeId)),
+    );
+  }
+
+  getDistance(): Observable<number> {
+    return this.location$.pipe(
+      switchMap((location) => this.geolocation.locationDistance(location))
     );
   }
 
