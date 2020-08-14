e33639faf78693950f241fa5e5f49a69abb0f19a
nvz838 <nvz838@gmail.com>
2020-08-14 12:42:54 +1200

    create email register page

---
+++ stem-explorer-ng/src/app/containers/register-email/register-email.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-email',
+  templateUrl: './register-email.component.html',
+  styleUrls: ['./register-email.component.scss']
+})
+export class RegisterEmailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
