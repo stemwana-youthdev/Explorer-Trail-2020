c07c9ff3638205ed8c2d0198bba31751324e6e4a
nvz838 <nvz838@gmail.com>
2020-08-15 10:03:29 +1200

    check fields for errors

--- stem-explorer-ng/src/app/containers/register-email/register-email.component.ts
+++ stem-explorer-ng/src/app/containers/register-email/register-email.component.ts
@@ -7,9 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterEmailComponent implements OnInit {
 
+  emailValue = '';
+  passwordValue = '';
+  confirmPasswordValue = '';
+
+  emailField = 'profile';
+  passwordField = 'profile';
+  confirmField = 'profile';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  register(email: string, password: string, confirmPassword: string) {
+    if (this.checkEmailFormat(email) && this.checkPasswordFormat(password) && this.checkPasswordMatch(password, confirmPassword)
+     && email !== '' && password !== '' && confirmPassword !== '') {
+       console.log('successful register');
+     } else {
+       console.log('error');
+     }
+  }
+
+  checkEmailFormat(email: string) {
+    if (/.+@+.+\.+./.test(email) || email === '') {
+      this.emailField = 'profile';
+      return true;
+    } else {
+      this.emailField = 'profile error';
+      return false;
+    }
+  }
+
+  checkPasswordFormat(password: string) {
+    // tslint:disable-next-line: max-line-length
+    if (/^(?=^.{8,}$)(?=.*?\d.*\d)(?=.*?[A-Z].*[A-Z])(?=.*?[a-z].*[a-z])(?=.*?[!@#$%^&;*()_+}{:'"?/.,].*[!@#$%^&;*()_+}{:'"?/.,])(?!.*\s).*/.test(password)
+     || password === '') {
+      this.passwordField = 'profile';
+      return true;
+    } else {
+      this.passwordField = 'profile error';
+      return false;
+    }
+  }
+
+  checkPasswordMatch(password: string, confirmPassword: string) {
+    if (password === confirmPassword || confirmPassword === '') {
+      this.confirmField = 'profile';
+      return true;
+    } else {
+      this.confirmField = 'profile error';
+      return false;
+    }
   }
 
 }
