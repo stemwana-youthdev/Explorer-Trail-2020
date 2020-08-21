73779178574f96d0ab772138e1ba4a12e5d38cd9
Benjamin-Davies <bentendavies@gmail.com>
2020-08-22 08:29:05 +1200

    Move registerUser call out of constructor

--- stem-explorer-ng/src/app/shared/auth/auth.service.ts
+++ stem-explorer-ng/src/app/shared/auth/auth.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { ReplaySubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { auth } from 'firebase/app';
 import 'firebase/auth';
 
 import { ApiService } from '../services/api.service';
 import { User } from '../models/user';
-import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -31,50 +31,7 @@ export class AuthService {
     this.afAuth.authState.subscribe(async (state) => {
       const token = await state?.getIdToken();
       this.api.token = token;
-
-      if (!token) {
-        return;
-      }
-
-      let user = await this.api.getCurrentUser().toPromise();
-
-      if (!user) {
-        // TODO: prompt user for registration info
-
-        const [firstName, lastName] = this.splitName(state.displayName);
-
-        const userInfo: User = {
-          // id will be ignored
-          id: null,
-          firstName,
-          lastName,
-          region: '',
-          homeTown: '',
-        };
-
-        user = await this.api.registerUser(userInfo).toPromise();
-      }
-
-      console.log('User logged in with backend!', user);
     });
-  }
-
-  // Split first and last name
-  splitName(displayName: string): [string, string] {
-    if (typeof displayName !== 'string') {
-      return ['', ''];
-    }
-    const parts = displayName.split(' ');
-    if (parts.length <= 1) {
-      // Just one word is normally a first name
-      return [displayName, ''];
-    } else {
-      // Otherwise one last name and the rest are first names
-      return [
-        parts.slice(0, parts.length - 1).join(' '),
-        parts[parts.length - 1],
-      ];
-    }
   }
 
   // google signin
@@ -85,7 +42,21 @@ export class AuthService {
   async authLogin(provider: auth.AuthProvider) {
     try {
       const res = await this.afAuth.signInWithPopup(provider);
-      console.log('You have been succesfully logged in! woohoo', res);
+
+      let user = await this.api.getCurrentUser().toPromise();
+      if (!user) {
+        const userInfo: User = {
+          // id will be ignored
+          id: null,
+          firstName: '',
+          lastName: '',
+          region: '',
+          homeTown: '',
+        };
+        user = await this.api.registerUser(userInfo).toPromise();
+      }
+
+      console.log('You have been succesfully logged in! woohoo', res, user);
     } catch (error) {
       console.warn(error);
     }
