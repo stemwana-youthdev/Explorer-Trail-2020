1ab21edb0cb04de228707e03c40a51d23af79d43
nvz838 <nvz838@gmail.com>
2020-08-23 16:19:02 +1200

    update profile in db

--- stem-explorer-ng/src/app/containers/profile/profile.component.ts
+++ stem-explorer-ng/src/app/containers/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-profile',
@@ -7,6 +9,8 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+
+  user: User;
 
   profileForm = new FormGroup({
     firstName: new FormControl(''),
@@ -16,13 +20,28 @@ export class ProfileComponent implements OnInit {
     homeTown: new FormControl(''),
   });
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.getUser();
   }
 
-  onSubmit() {
-    console.log(this.profileForm.value);
+  async getUser() {
+    await this.auth.getCurrentUser().then(value =>
+      this.user = value
+    );
+    this.profileForm.get('firstName').setValue(this.user.firstName);
+    this.profileForm.get('lastName').setValue(this.user.lastName);
+    this.profileForm.get('region').setValue(this.user.region);
+    this.profileForm.get('homeTown').setValue(this.user.homeTown);
+  }
+
+  async onSubmit() {
+    this.user.firstName = this.profileForm.get('firstName').value;
+    this.user.lastName = this.profileForm.get('lastName').value;
+    this.user.region = this.profileForm.get('region').value;
+    this.user.homeTown = this.profileForm.get('homeTown').value;
+    await this.auth.updateCurrentUser(this.user);
   }
 
 }
