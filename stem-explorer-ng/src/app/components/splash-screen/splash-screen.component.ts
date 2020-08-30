e9ac1675829badcae6d3af221b7bcb7c7c37f090
Harry Parkes <harrydparkes@protonmail.com>
2020-08-30 14:53:03 +1200

    adding T&Cs

--- stem-explorer-ng/src/app/components/splash-screen/splash-screen.component.ts
+++ stem-explorer-ng/src/app/components/splash-screen/splash-screen.component.ts
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashScreenComponent implements OnInit {
 
-  title = 'Welcome to the STEM Explorer Trail';
-  content = 'welcome content';
+  title = 'Are you ready to explore?';
+  content = 'Unlock hidden science, technology, engineering, and mathematics (STEM) challenges and tasks within Tauranga’s CBD. The STEM Explorer Trail is a one of a kind app experience made by locals for our locals and visitors. Complete challenges as an individual or a team, unlock special tasks, explore our CBD and win some cool prizes!';
+  TnCs = 'By using this website/app you agree to our Terms & Conditions.'
 
   constructor() { }
 
