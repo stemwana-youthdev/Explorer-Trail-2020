2c527e7b454a2d69d58c01544c781da52b45c8c7
Harry Parkes <harrydparkes@protonmail.com>
2020-09-03 18:23:11 +1200

    Added T&Cs to splsh screen

--- stem-explorer-ng/src/app/components/splash-screen/splash-screen.component.ts
+++ stem-explorer-ng/src/app/components/splash-screen/splash-screen.component.ts
@@ -8,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SplashScreenComponent implements OnInit {
 
   title = 'Are you ready to explore?';
-  content = 'Unlock hidden science, technology, engineering, and mathematics (STEM) challenges and tasks within Tauranga’s CBD. The STEM Explorer Trail is a one of a kind app experience made by locals for our locals and visitors. Complete challenges as an individual or a team, unlock special tasks, explore our CBD and win some cool prizes!';
-  TnCs = 'By using this website/app you agree to our Terms & Conditions.'
+  content = 'Unlock hidden science, technology, engineering, and mathematics (STEM) challenges and tasks within Tauranga’s CBD. The STEM Explorer Trail is a one of a kind app experience made by locals for our locals and visitors. Complete challenges as an individual or a team, explore our CBD and win some cool prizes!';
 
   constructor() { }
 
