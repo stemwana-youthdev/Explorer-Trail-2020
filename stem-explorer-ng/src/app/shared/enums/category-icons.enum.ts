78cd6dc31c5d454532d4459d61af2a7c29012d05
Benjamin-Davies <bentendavies@gmail.com>
2020-09-09 10:33:57 +1200

    Change enum to be more like the map icon one

--- stem-explorer-ng/src/app/shared/enums/category-icons.enum.ts
+++ stem-explorer-ng/src/app/shared/enums/category-icons.enum.ts
@@ -1,6 +1,6 @@
 export enum CategoryIcons {
-  '/assets/icons/CAT-science.svg',
-  '/assets/icons/CAT-technology.svg',
-  '/assets/icons/CAT-engineering.svg',
-  '/assets/icons/CAT-maths.svg',
+  'CAT-science.svg',
+  'CAT-technology.svg',
+  'CAT-engineering.svg',
+  'CAT-maths.svg',
 }
