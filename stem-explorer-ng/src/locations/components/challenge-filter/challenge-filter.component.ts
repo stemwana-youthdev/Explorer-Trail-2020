5894ae758ec999f58b7b186164fe703b13f38d9c
Benjamin-Davies <bentendavies@gmail.com>
2020-09-09 16:19:07 +1200

    Use large icons enum

--- stem-explorer-ng/src/locations/components/challenge-filter/challenge-filter.component.ts
+++ stem-explorer-ng/src/locations/components/challenge-filter/challenge-filter.component.ts
@@ -3,8 +3,8 @@ import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { FilterLocations } from 'src/locations/store/locations.actions';
 import { LocationsState } from 'src/locations/store/locations.state';
-import { CategoryIcons } from 'src/app/shared/enums/category-icons.enum';
 import { Categories } from 'src/app/shared/enums/categories.enum';
+import { LargeCategoryIcons } from 'src/app/shared/enums/large-category-icons.enum';
 
 @Component({
   selector: 'app-challenge-filter',
@@ -15,7 +15,7 @@ export class ChallengeFilterComponent implements OnInit {
   @Select(LocationsState.locationFilter) public filter$: Observable<number[]>;
 
   Categories = Categories;
-  CategoryIcons = CategoryIcons;
+  CategoryIcons = LargeCategoryIcons;
 
   buttons = [
     {category: 'S', value: 0, colorClass: 'green'},
