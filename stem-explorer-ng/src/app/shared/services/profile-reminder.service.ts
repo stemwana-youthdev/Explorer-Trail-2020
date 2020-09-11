ddec6e9d482a939480f63ccad26ecfe6c659e194
Benjamin-Davies <bentendavies@gmail.com>
2020-09-11 15:53:34 +1200

    Show a quick explanatory message

--- stem-explorer-ng/src/app/shared/services/profile-reminder.service.ts
+++ stem-explorer-ng/src/app/shared/services/profile-reminder.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable()
 export class ProfileReminderService {
-  constructor() {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private snackbar: MatSnackBar,
+  ) {
     this.checkCompleted();
   }
 
-  async checkCompleted() {
+  private async checkCompleted() {
     if (!this.auth._user) {
       return;
     }
@@ -23,9 +30,18 @@ export class ProfileReminderService {
     }
   }
 
-  remindUser() {
+  // This can be used anywhere to prompt the user to give us their details
+  public remindUser() {
     this.router.navigateByUrl('/profile');
     localStorage.setItem('lastProfileReminder', new Date().toISOString());
+
+    // Show a quick explanatory message for 10 seconds.
+    /** @todo use message service when it is implemented */
+    this.snackbar.open(
+      'Please take the time to give us some of your details. It helps us make STEM more engaging for everyone.',
+      null,
+      { duration: 10000 }
+    );
   }
 }
 
