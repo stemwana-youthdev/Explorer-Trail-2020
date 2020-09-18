0823ec93b84fb9fd7ae52302aabbc894502bbdf2
Benjamin-Davies <bentendavies@gmail.com>
2020-09-18 20:49:38 +1200

    Add confirm dialog

--- stem-explorer-ng/src/app/shared/guards/dirty-form.guard.ts
+++ stem-explorer-ng/src/app/shared/guards/dirty-form.guard.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { CanDeactivate } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ConfirmDialogComponent } from '../components/confirm-dialog/confirm-dialog.component';
 
 export interface CanLeave {
   canLeave(): boolean;
@@ -9,7 +13,20 @@ export interface CanLeave {
   providedIn: 'root',
 })
 export class DirtyFormGuard implements CanDeactivate<CanLeave> {
-  canDeactivate(component: CanLeave): boolean {
-    return component.canLeave();
+  constructor(private dialog: MatDialog) {}
+
+  canDeactivate(component: CanLeave): boolean | Observable<boolean> {
+    return component.canLeave() || this.confirmDialog();
+  }
+
+  confirmDialog(): Observable<boolean> {
+    const message =
+      'You have unsaved changes, are you sure you want to navigate away?';
+
+    const dialog = this.dialog.open(ConfirmDialogComponent, {
+      data: { message },
+      panelClass: 'app-dialog',
+    });
+    return dialog.afterClosed();
   }
 }
