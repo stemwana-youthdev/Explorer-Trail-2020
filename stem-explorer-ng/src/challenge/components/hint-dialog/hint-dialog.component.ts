65e7ebe95492b47fe91e0ee27b9267169ff82ebc
Kelly <kelly.williams@cucumber.co.nz>
2020-09-26 18:02:07 +1200

    fix hint to accept html tags

--- stem-explorer-ng/src/challenge/components/hint-dialog/hint-dialog.component.ts
+++ stem-explorer-ng/src/challenge/components/hint-dialog/hint-dialog.component.ts
@@ -9,7 +9,7 @@ import { StemColours } from 'src/app/shared/enums/stem-colours.enum';
     <app-dialog [category]="data.category">
       <h3 class="title">{{data.title}}</h3>
       <p>{{Level[data.level.difficulty]}}</p>
-      <p>{{data.level.hint}}</p>
+      <p [innerHTML]="data.level.hint"></p>
     </app-dialog>
   `,
   styles: ['.title { padding-right: 30px; }']
