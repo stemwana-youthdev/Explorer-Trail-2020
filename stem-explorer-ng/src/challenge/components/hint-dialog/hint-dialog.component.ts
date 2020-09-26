292a03b083cb83e6a664dca03cb3618fce1b52a3
Benjamin-Davies <bentendavies@gmail.com>
2020-09-27 09:59:45 +1300

    Show links in hint text as links

--- stem-explorer-ng/src/challenge/components/hint-dialog/hint-dialog.component.ts
+++ stem-explorer-ng/src/challenge/components/hint-dialog/hint-dialog.component.ts
@@ -9,7 +9,7 @@ import { StemColours } from 'src/app/shared/enums/stem-colours.enum';
     <app-dialog [category]="data.category">
       <h3 class="title">{{data.title}}</h3>
       <p>{{Level[data.level.difficulty]}}</p>
-      <p [innerHTML]="data.level.hint"></p>
+      <p [innerHTML]="hintHTML"></p>
     </app-dialog>
   `,
   styles: ['.title { padding-right: 30px; }']
@@ -19,4 +19,15 @@ export class HintDialogComponent {
   Colour = StemColours;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  get hintHTML() {
+    const originalHint: string = this.data.level.hint;
+    const withLinks = originalHint.replace(
+      // Regex from https://regexr.com/39nr7
+      // Select links such as 'http://test.com/hi', 'example.nz' and 'https://youtu.be/sdlkajldkfjas'
+      /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
+      (linkUrl) => `<a href="${linkUrl}" target="_blank">${linkUrl}</a>`
+    );
+    return withLinks;
+  }
 }
