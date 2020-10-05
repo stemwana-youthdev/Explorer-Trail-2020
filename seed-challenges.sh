83a6a15af560190b25fcdc73b69cea15c7ec06fd
Kelly <kelly.williams@cucumber.co.nz>
2020-10-05 14:27:06 +1300

    update separate seed files with prod data

--- seed-challenges.sh
+++ seed-challenges.sh
@@ -11,15 +11,23 @@ psql -d StemExplorer -U stem << EOF
 DELETE FROM "Challenges";
 INSERT INTO "Challenges" ("Id","Title","Description","Category","LocationId")
 VALUES 
-(5, 'Identify the Mixture', 'Learning about different types of mixtures', 0, 7),
-(7, 'Identifying Directions', 'Learning how to identify direction by using the concept of a sundial', 0, 16),
-(8, 'Learning about Rock Types', 'Understanding the Type of Rocks and how they are formed', 0, 3),
-(9, 'Identify the State of Matter', 'Learning more about the states of matter', 0, 13),
-(10, 'Identify Railway parts', 'Learning about the parts of a Railway and its functions', 2, 15),
-(11, 'Identify the Type of Bridge', 'Learning about types of bridges an why they are designed that way', 2, 6),
-(12, 'Understanding pH', 'Learning how to determine the pH of certain substances', 0, 2),
-(13, 'Soil pH', 'Understanding the effects of soil pH', 0, 2),
-(14, 'Identify the parts of the flower', 'Understanding the different parts of a flower and its functions', 0, 7),
-(15, 'Identify the angles', 'Learning how to identify angles based on its measurement', 3, 8),
-(17, 'The Science of Clouds', 'Understanding the Science behind the formation of clouds and classifying different types of clouds', 0, 14);
+(1,'Find the Location','Navigation using coordinates',1,12),
+(2,'Converting to Binary','Learning to convert text to Binary - a fundamental concept in coding',1,1),
+(3,'Understanding Forces','Understand the forces in our everyday lives',0,5),
+(4,'The Golden Ratio','Learn the basic concepts of the Golden Ratio and what is it used for',3,10),
+(5,'Identify the Mixture','Learning about different types of mixtures',0,7),
+(6,'Measurement','Learning how to measure height of a tree without actually climbing it',3,9),
+(7,'Identifying Directions','Learning how to identify direction by using the concept of a sundial',0,16),
+(8,'Learning about Rock Types','Understanding the type of rocks and how they are formed',0,3),
+(9,'Identify the State of Matter','Learning more about the states of matter',0,13),
+(10,'Identify Railway parts','Learning about the parts of a railway and its functions',2,15),
+(11,'Identify the Type of Bridge','Learning about types of bridges and why they are designed that way',2,6),
+(12,'Understanding pH','Learning how to determine the pH of certain substances',0,2),
+(13,'Soil pH','Understanding the effects of soil pH',0,2),
+(14,'Identify the parts of the flower','Understanding the different parts of a flower and their functions',0,7),
+(15,'Identify the angles','Learning how to identify angles based on their measurements',3,8),
+(16,'Mystery of Automatic Doors','Understanding how automatic doors were made and how they operate',1,4),
+(17,'The Science of Clouds','Understanding the science behind the formation of clouds and how to classify different types of clouds',0,14),
+(18,'Stormwater Management','Understanding stormwater',0,11),
+(19,'Raingarden','Understanding how a raingarden works',2,11);
 EOF
--- seed-locations.sh
+++ seed-locations.sh
@@ -9,18 +9,37 @@ docker exec -i explorer-trail-2020_explorer_trail_db_1 \
 psql -d StemExplorer -U stem << EOF
 
 DELETE FROM "Locations";
-
-INSERT INTO "Locations" ("LocationId", "Name", "Latitude", "Longitude", "Url", "GooglePlaceId", "Phone", "Email")
+INSERT INTO "Locations" ("LocationId", "Name", "Latitude", "Longitude", "Url", "GooglePlaceId", "Phone", "Email", "Address")
 VALUES 
-(2, 'STEM HQ','-37.6852274','176.169554','https://stemwana.nz','ChIJcbv3p8vbbW0RDh2xBjVnW5Y',NULL,NULL),
-(3, 'Tauranga City Library','-37.6828074','176.168648','https://library.tauranga.govt.nz/','ChIJD5sBYefbbW0RP-oj-NNSuoA',NULL,NULL),
-(6, 'Tauranga Harbour Bridge','-37.6868384','176.170551',NULL,NULL,NULL,NULL),
-(7, 'Robbins Park (Rose Garden)','-37.6785922','176.16993','https://nzroses.org.nz/robbins-park-tauranga/','ChIJn-qg2v_bbW0RvJdrRFj9ICU',NULL,NULL),
-(8, 'The Red Square','-37.6843611','176.169493',NULL,'ChIJnQmXE93bbW0RJJfvr-mVObw',NULL,NULL),
-(11, 'Trustpower','-37.6852996','176.167088','https://www.trustpower.co.nz/','ChIJgxvxUurYbW0RuC2eHkOaUQA',NULL,NULL),
-(12, 'Tourism BOP i-Site','-37.6834958','176.169408','https://www.bayofplentynz.com/','ChIJn9OKit3bbW0ROSguplmSk5U',NULL,NULL),
-(13, 'ANZ','-37.684608','176.168302',NULL,'ChIJd11cu93bbW0RNOIA1Gw6-jU',NULL,NULL),
-(14, 'The Elms','-37.6769001','176.167888','https://www.theelms.org.nz/','ChIJZztOMeTbbW0RwatcxISscI8',NULL,NULL),
-(15, 'Angel Wings Mural','-37.6840611','176.169742',NULL,'ChIJe0DLC97bbW0RDf9jo_ssV1g',NULL,NULL),
-(16, 'Tauranga Seafront','-37.6828459','176.169782',NULL,NULL,NULL,NULL);
+(1,'Basestation','-37.686501','176.167175','https://www.basestation.nz','ChIJ905i8dzbbW0RIvN_2Wp8sJQ','0800 000 577',NULL,'148 Durham Street Tauranga 3110'),
+
+(2,'STEM HQ','-37.685192','176.169379','https://stemwana.nz','ChIJcbv3p8vbbW0RDh2xBjVnW5Y',NULL,NULL,'27 Devonport Road, Tauranga 3110'),
+
+(3,'Tauranga City Library','-37.683150','176.168412','https://library.tauranga.govt.nz/','ChIJD5sBYefbbW0RP-oj-NNSuoA','6475777177',NULL,'91 Willow Street, Tauranga 3110'),
+
+(4,'Tauranga Art Gallery','-37.683226','176.169094','https://www.artgallery.org.nz/','ChIJ8TBZ9N3bbW0RvpRzvjAWLkY','6475787933',NULL,'108 Willow Street, Tauranga 3141'),
+
+(5,'Playground at The Strand','-37.682759','176.170447',NULL,'ChIJiZqcT-XbbW0RkghoF1isOOQ',NULL,NULL,'78 The Strand, Tauranga 3110'),
+
+(6,'Matapihi Railwaybridge','-37.687021','176.170609',NULL,NULL,NULL,NULL,'150 The Strand, Tauranga 3110'),
+
+(7,'Robbins Park (Rose Garden)','-37.678620','176.169868','https://nzroses.org.nz/robbins-park-tauranga/','ChIJn-qg2v_bbW0RvJdrRFj9ICU',NULL,NULL,'7 Cliff Road, Tauranga 3110'),
+
+(8,'Red Square','-37.684393','176.169347',NULL,'ChIJnQmXE93bbW0RJJfvr-mVObw',NULL,NULL,'Spring Street, Tauranga 3110'),
+
+(9,'Memorial Park','-37.698406','176.165399','https://www.tauranga.govt.nz/exploring/parks-and-reserves/parks/memorial-park','ChIJMaygGc7bbW0R0B6dJGLvAA8',NULL,NULL,'Eleventh Avenue, Tauranga 3110'),
+
+(10,'University of Waikato','-37.685510','176.167004','https://tauranga.waikato.ac.nz/','ChIJj3H6avzbbW0RULwvrR5XC34',NULL,NULL,'113 Durham Street, Tauranga 3110'),
+
+(11,'Trustpower','-37.685489','176.167390','https://www.trustpower.co.nz/','ChIJgxvxUurYbW0RuC2eHkOaUQA','64800878787',NULL,'108 Durham Street, Tauranga 3110'),
+
+(12,'Tourism BOP i-Site','-37.683588','176.169919','https://www.bayofplentynz.com/','ChIJn9OKit3bbW0ROSguplmSk5U','6475788103',NULL,'103 The Strand, Tauranga 3110'),
+
+(13,'ANZ Bank','-37.684647','176.168299',NULL,'ChIJd11cu93bbW0RNOIA1Gw6-jU',NULL,NULL,'1 Grey Street, Tauranga 3110'),
+
+(14,'The Elms','-37.676884','176.168024','https://www.theelms.org.nz/','ChIJZztOMeTbbW0RwatcxISscI8','6475779772',NULL,'15 Mission Street, Tauranga 3110'),
+
+(15,'Angel Wings Mural','-37.684104','176.170213',NULL,'ChIJe0DLC97bbW0RDf9jo_ssV1g',NULL,NULL,'117 The Strand, Tauranga 3110'),
+
+(16,'Tauranga Seafront','-37.682977','176.170535',NULL,'ChIJ6-7mp-XbbW0RrtFGblLrm1Q',NULL,NULL,'80 The Strand, Tauranga 3110');
 EOF
