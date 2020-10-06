68babadb742b97aa1678932f87895da20257ea83
Benjamin-Davies <bentendavies@gmail.com>
2020-10-06 18:05:20 +1300

    Add db for videos

---
+++ StemExplorerAPI/StemExplorerAPI/Migrations/20201006045824_AddVideoEmbedUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StemExplorerAPI.Migrations
+{
+    public partial class AddVideoEmbedUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VideoEmbedUrl",
+                table: "ChallengeLevels",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VideoEmbedUrl",
+                table: "ChallengeLevels");
+        }
+    }
+}
